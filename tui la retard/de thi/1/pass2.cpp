2c70a6fd412fce2794b4b25e5221dc13e0d8c8cd
nck404 <khoavencyti@gmail.com>
2023-12-24 10:35:22 +0700

    cac

---
+++ tui la retard/de thi/1/pass2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int sod(long long num) {
+    long long sum = 0;
+    while (num != 0) {
+        sum += num % 10;  
+        num /= 10;        
+    }
+    return sum;
+}
+int main() {
+
+    ifstream infile("PASSWORD.INP");
+      int N = 1000;
+      bool check[N + 1];
+      for (int i = 2; i <= N; i++) {
+        check[i] = true;
+      }
+
+      for (int i = 2; i <= N; i++) {
+        if (check[i] == true) {
+          for (int j = 2 * i; j <= N; j += i) {
+            check[j] = false;
+          }
+        }
+      }
+
+
+    long long l, r;
+    infile >> l >> r;
+    infile.close();
+    ofstream outfile("PASSWORD.OUT");
+    for (long long i = l; i < r; ++i)
+    {
+        if (check[i] == true && check[sod(i)] == true) {
+            outfile << i << ' ';
+        }
+    }
+    outfile.close();
+
+    return 0;
+}
